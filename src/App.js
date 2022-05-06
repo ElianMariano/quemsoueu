574996b9a52a5a55fed1264089822c3de2179070
Elian Mariano <elianmarianogabriel@gmail.com>
2022-05-06 19:19:26 -0300

    Initial project structure and Header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './global.css';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header/>
+
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+function Header(){
+    return (
+        <header>
+            <h1>Quemsoueu</h1>
+
+            <nav>
+                <ul>
+                    <li>Minha História</li>
+                    <li>Meus Interesses</li>
+                    <li>Minha Experiência</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
